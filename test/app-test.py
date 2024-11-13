70bc2c9913cd733986a5640a55104caf294a6d2e
SAM <11062003sam@gmail.com>
2024-11-13 12:24:02 +0530

    Add test for /hello endpoint

--- test/app-test.py
+++ test/app-test.py
@@ -1,13 +1,18 @@
 import unittest
 from app import app
 
-class TestHelloWorld(unittest.TestCase):
-    def setUp(self):
-        self.app = app.test_client()
+class HelloTestCase(unittest.TestCase):
+    def setUp(self):
+        self.app = app.test_client()
+        self.app.testing = True
 
-    def test_hello(self):
-        response = self.app.get('/hello')
-        self.assertEqual(response.data.decode(), "Hello, World!")
+    def test_hello_without_name(self):
+        response = self.app.get('/hello')
+        self.assertEqual(response.data.decode(), "Hello, World!")
+    
+    def test_hello_with_name(self):
+        response = self.app.get('/hello?name=John')
+        self.assertEqual(response.data.decode(), "Hello, John!")
 
 if __name__ == '__main__':
-    unittest.main()
+    unittest.main() 
